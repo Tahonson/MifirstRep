1152fc579ddeb38e7b6af319e9efa39fcebf1fed
Sokolov Denis <lasker11@yandex.ru>
2017-10-11 02:37:34 +0300

    Дз 2

--- Homework2/functions.php
+++ Homework2/functions.php
@@ -1,1 +1,18 @@
 <?php
+//task 1
+
+function task1($string, $true)
+{
+    if ($true == true) {
+        echo implode(" ", $string);
+        return $string;
+    } else {
+        $arr = explode(" ", $string);
+        $count = count($arr);
+        for ($i=0;$i<=$count;$i++) {
+            echo $arr[$i] ."<p>";
+        }
+    }
+
+}
+task1("фвцзвфц вцфзщ фц вфц вфцвфцвфц вфц", 12);
