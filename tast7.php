fe886a95728087fcbb175859dace2c44334085cd
Sokolov Denis <lasker11@yandex.ru>
2017-10-11 00:51:42 +0300

    Дз 1

--- tast7.php
+++ tast7.php
@@ -1,16 +1,16 @@
 <?php
 echo "<table border='1px'>";
 $a = array();
-$m = 1;
 for ($i=1;$i<=10;$i++) {
     echo "<tr>";
     for($j=1;$j<=10;$j++) {
         echo "<td>";
         $a[$i][$j]=$i*$j;
-        if ($i%2 == 0 and $j%2 == 0) {
+        $m=$i+1;
+        if ($m%2 == 1 and $j%2 == 0) {
             echo "(" .$a[$i][$j] .")";
         }       else echo "[" .$a[$i][$j] ."]";
-        $m++;
+
         echo "</td>";
     }
     echo "</tr>";
