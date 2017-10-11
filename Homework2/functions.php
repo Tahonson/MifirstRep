147543f3c20b1a06886fb0fbe253c7d88c444a15
Sokolov Denis <lasker11@yandex.ru>
2017-10-11 12:30:59 +0300

    Дз 2

--- Homework2/functions.php
+++ Homework2/functions.php
@@ -16,3 +16,15 @@ function task1($string, $true)
 
 }
 task1("В чем проблема - не знаю что с первым условием не так", 1);
+
+//task2
+
+function tast2($array, $string) {
+  //  $newarray = implode ("$string", $array);
+    $elements = count ($array);
+    for ($i=0;$i<=$elements;$i++) {
+
+    }
+
+
+}
