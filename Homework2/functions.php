304c2f7e79ef62cff8f9aa23ffd34765b29c2981
Sokolov Denis <lasker11@yandex.ru>
2017-10-11 18:23:10 +0300

    Дз 2

--- Homework2/functions.php
+++ Homework2/functions.php
@@ -1,5 +1,5 @@
 <?php
-//task 1
+//task 1 Задание №1
 
 function task1($string, $true)
 {
@@ -17,14 +17,40 @@ function task1($string, $true)
 }
 task1("В чем проблема - не знаю что с первым условием не так", 1);
 
-//task2
+//task2 Задание № 2
 
 function tast2($array, $string) {
-  //  $newarray = implode ("$string", $array);
-    $elements = count ($array);
-    for ($i=0;$i<=$elements;$i++) {
-
+    /* //работа со строкой ( введеной)//
+    $newarray = explode (" ", $array); */
+//$elements = count ($array);
+//foreach ($newarray as $elem) {
+    $resault =0;
+    foreach ($array as $elem ) {
+        switch ($string) {
+            case '+':
+                $resault = $resault + $elem;
+                break;
+            case '-':
+                $resault = $resault - $elem;
+                break;
+            case '*':
+                $resault1 = $resault1 * $elem;
+                $resault = $resault1;
+                break;
+            case '/':
+                $resault1 = $resault1 / $elem;
+                $resault = $resault1;
+                break;
+        }
     }
+    return $resault;
+    echo $resault;
+}
+$rar = array(1,2,3,4,5);
+echo tast2( $rar,"+");
 
+//task3 Задание № 3
+
+function tast3() {
 
 }
