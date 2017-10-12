6503dc09b6e5a91875c1a77ac5ed8ddabf444fa7
Sokolov Denis <lasker11@yandex.ru>
2017-10-12 08:24:55 +0300

    Дз 2

--- Homework2/functions.php
+++ Homework2/functions.php
@@ -1,6 +1,6 @@
 <?php
 //task 1 Задание №1
-
+echo "--------------Task1------------------- <br>";
 function task1($string, $true)
 {
     if ($true === true) {
@@ -9,24 +9,26 @@ function task1($string, $true)
     } else {
         $arr = explode(" ", $string);
         $count = count($arr);
-        for ($i=0;$i<=$count;$i++) {
-            echo $arr[$i] ."<p>";
+        for ($i = 0; $i <= $count; $i++) {
+            echo $arr[$i] . "<p>";
         }
     }
 
 }
-//task1("В чем проблема - не знаю что с первым условием не так", 1);
+
+task1("В чем проблема - не знаю что с первым условием не так", 1);
 
 //task2 Задание № 2
-
-function tast2($array, $string) {
+echo "------------------Task2--------------- <br>";
+function task2($array, $string)
+{
     /* //работа со строкой ( введеной)//
     $newarray = explode (" ", $array); */
 //$elements = count ($array);
 //foreach ($newarray as $elem) {
-    $resault =0;
-    $resault1=1;
-    foreach ($array as $elem ) {
+    $resault = 0;
+    $resault1 = 1;
+    foreach ($array as $elem) {
         switch ($string) {
             case '+':
                 $resault = $resault + $elem;
@@ -42,16 +44,46 @@ function tast2($array, $string) {
                 $resault1 = $resault1 / $elem;
                 $resault = $resault1;
                 break;
+            default:
+                return 'Неизвестный арифметический оператор';
+                break;
         }
     }
     return $resault;
-    echo $resault;
 }
-$rar = array(1,2,3,4,5,6,6,7,7,7);
-echo tast2( $rar,"+");
+
+$rar = array(1, 2, 3, 4, 5, 6, 6, 7, 7, 7);
+echo task2($rar, "+");
 
 //task3 Задание № 3
-
-function tast3() {
-
+echo "<br>------------------Task3--------------- <br>";
+function task3(...$numbers)
+{
+    $kolvo = func_num_args();
+    $operator = func_get_arg(0);
+    if ($operator == '+') {
+        $tusk3 = 0;
+        for ($i = 1; $i <= $kolvo; $i++) {
+            $tusk3 = $tusk3 + $operator($i);
+        }
+    } elseif ($operator == '-') {
+        $tusk3 = 0;
+        for ($i = 1; $i <= $kolvo; $i++) {
+            $tusk3 = $tusk3 - $operator($i);
+        }
+    } elseif ($operator == '*') {
+        $tusk3 = 1;
+        for ($i = 1; $i <= $kolvo; $i++) {
+            $tusk3 = $tusk3 * $operator($i);
+        }
+    } elseif ($operator == '/') {
+        $tusk3 = 1;
+        for ($i = 1; $i <= $kolvo; $i++) {
+            $tusk3 = $tusk3 / $operator($i);
+        }
+    } else echo "Введите первым символом математическую операцию";
+    return $tusk3;
 }
+
+
+echo task3("- 1 2 3 4 5 6 ");
