cac6602d1d627ae4aeb38d88b840ba9494b3cc25
Sokolov Denis <lasker11@yandex.ru>
2017-10-12 22:27:10 +0300

    Дз 2

--- Homework2/functions.php
+++ Homework2/functions.php
@@ -84,6 +84,65 @@ function task3(...$numbers)
     } else echo "Введите первым символом математическую операцию";
     return $tusk3;
 }
-
-
 echo task3("- 1 2 3 4 5 6 ");
+
+echo "<br>------------------Task4--------------- <br>";
+
+function task4($p1,$p2) {
+    if (is_integer($p1) and is_integer($p2))
+    {
+        echo "<table border='1px'>";
+        for ($i=1;$i<=$p1;$i++) {
+
+            echo "<tr>";
+
+            for($j=1;$j<=$p2;$j++) {
+
+                echo "<td>";
+
+                $a[$i][$j]=$i*$j;
+
+                $m=$i+1;
+                echo $a[$i][$j];
+                echo "</td>";
+
+            }
+
+            echo "</tr>";
+
+        }
+
+        echo "</table>";
+
+
+    } else echo "неверно введены данные";
+
+}
+echo task4(10,"ff");
+
+echo "<br>------------------Task5--------------- <br>";
+function isPalindrome($w){
+    for($i = 0, $l = strlen($w)-1, $il = ceil($l/2); $i < $il; ++$i) {
+        if($w[$i] != $w[$l-$i]) return false;
+        return true;
+    }
+}
+echo "<br>------------------Task6--------------- <br>";
+
+function task6() {
+    echo date('d.m.Y H:i') ."
+";
+    echo strftime '24.02.2016 00:00:00';
+}
+echo task6();
+
+echo "<br>------------------Task7--------------- <br>";
+function task7() {
+    $one ='Карл у Клары украл Кораллы';
+    echo $string= str_replace('К', '',$one) ."
+";
+    $two ='Две бутылки лимонада';
+    echo $string = str_replace ('Две', 'Три', $two);
+}
+echo task7();
+echo "<br>------------------Task8--------------- <br>";
