19f279d768ecb2321039bd7c011cf9bca0cc636e
Sokolov Denis <lasker11@yandex.ru>
2017-10-14 17:24:30 +0300

    Дз 2

--- Homework2/index.php
+++ Homework2/index.php
@@ -1,12 +1,19 @@
 <?php
 require('functions.php');
 echo "<br>------------------Task1--------------- <br>";
+echo task1(array ('My', 'name', 'is', 'Zeus!'), falce);
 echo "<br>------------------Task2--------------- <br>";
+echo task2(array(1, 2, 3, 4,5), '+');
 echo "<br>------------------Task3--------------- <br>";
+echo task3('*', 625, 25, 5);
 echo "<br>------------------Task4--------------- <br>";
+echo task4(8, 8);
 echo "<br>------------------Task5--------------- <br>";
+echo task5('eng isi gne');
 echo "<br>------------------Task6--------------- <br>";
+echo task6();
 echo "<br>------------------Task7--------------- <br>";
+echo task7();
 echo "<br>------------------Task8--------------- <br>";
 echo "<br>------------------Task9--------------- <br>";
 echo "<br>------------------Task10-------------- <br>";
