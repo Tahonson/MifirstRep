906ca8b93eb4aff060c290add84ea0df24aad451
Sokolov Denis <lasker11@yandex.ru>
2017-10-16 18:33:11 +0300

    Дз 2

--- task3.php
+++
@@ -1,4 +0,0 @@
-<?php
-define(I,'я константа');
-if(defined("I")==true) echo "Константа I объявлена <br>";
-echo I;
--- tast1.php
+++
@@ -1,9 +0,0 @@
-<?php
-$name = Denis;
-$age = 21;
-echo "Меня зовут: $name <br>";
-echo "Мне $age год<br>";
-echo $b;
-echo ' "!|\/\'"\\  ';
-
-
